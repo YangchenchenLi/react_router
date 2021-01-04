f972a89699a18fa7cdf438e6f28d87dfb86f9e2c
Yangchenchen <yangchenchen.li@uq.net.au>
2021-01-04 20:29:31 +1000

    Codes update

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,44 +1,45 @@
-import React from 'react'
-import {Route, Switch, Redirect} from 'react-router-dom'
-import MyNavLink from './my-nav-link'
+import React, {Component} from 'react'
+import {NavLink, Switch, Route, Redirect} from "react-router-dom";
+
 import About from '../views/about'
 import Home from '../views/home'
 
-export default class App extends React.Component {
+export default class App extends Component {
 
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-            <div className="page-header">
-              <h2>React Router Demo</h2>
-            </div>
-          </div>
-        </div>
+    render(){
 
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              {/*导航路由链接*/}
-              <MyNavLink className="list-group-item" to='/about'>About</MyNavLink>
-              <MyNavLink className="list-group-item" to='/home'>Home</MyNavLink>
+        return(
+            <div>
+                <div className='row'>
+                    <div className='col-xs-offset-2 col-xs-8'>
+                        <div className='page-header'><h2>React Router Demo</h2></div>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='col-xs-2 col-xs-offset-2'>
+                        <div className='list-group'>
+                            <NavLink className='list-group-item' to='/about'>About</NavLink>
+                            <NavLink className='list-group-item' to='home'>Home</NavLink>
+                        </div>
+                    </div>
+
+                    <div className='col-xs-6'>
+                        <div className='panel'>
+                            <div className='panel-body'>
+                                <Switch>
+                                    <Route path='/about' component={About} />
+                                    <Route path='/home' component={Home} />
+                                    {/*一开始就自动定位到about里面*/}
+                                    <Redirect to='/about'/>
+                                </Switch>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                {/*可切换的路由组件*/}
-                <Switch>
-                  <Route path='/about' component={About}/>
-                  <Route path='/home' component={Home}/>
-                  <Redirect to='/about'/>
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
--- src/views/about.jsx
+++ src/views/about.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
-export default function About() {
-  return <div>About组件内容</div>
+import React, {Component} from 'react'
+
+export default class About extends Component {
+
+    render(){
+
+        return(
+            <div>
+                about route component
+            </div>
+        )
+    }
 }
